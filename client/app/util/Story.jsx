84a99ef211db0512ea9036e592af3bf0e237877a
unknown <tatitaye0@gmail.com>
2024-04-06 12:37:32 +0300

    noscroll

--- client/app/util/Story.jsx
+++ client/app/util/Story.jsx
@@ -1,7 +1,7 @@
 export default function Story(){
     return(
         <main>
-            hi
+            
         </main>
     )
 }
--- client/app/util/TimeLine.jsx
+++ client/app/util/TimeLine.jsx
@@ -1,19 +1,43 @@
 export default function Timeline() {
-  return (
-    <main>
-      <div className="hero min-h-screen bg-base-200">
-        <div className="hero-content text-center">
-          <div className="max-w-md">
-            <h1 className="text-5xl font-bold">Hello there</h1>
-            <p className="py-6">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-              et a id nisi.
-            </p>
-            <button className="btn btn-primary">Get Started</button>
+    return (
+      <main className="h-screen overflow-y-auto" style={{ scrollbarWidth: 'none' }}>
+        <div className="card w-full bg-base-100 shadow-xl mt-10 mb-10">
+          <button type="button" aria-label="Close" className="uk-close absolute top-2 right-2 z-10"></button>
+          <div className="flex-shrink-0 group block">
+            <div className="flex items-center">
+              <img
+                className="inline-block flex-shrink-0 w-12 h-12 rounded-full"
+                src="https://images.unsplash.com/photo-1568602471122-7832951cc4c5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=300&h=300&q=80"
+                alt="Image Description"
+              />
+              <div className="ms-3">
+                <h3 className="font-semibold text-gray-800 dark:text-white">
+                  Mark Wanner
+                </h3>
+                <p className="text-sm font-medium text-gray-400">mark@gmail.com</p>
+              </div>
+            </div>
           </div>
+          <div className="card-body">
+            <p className="text-base">
+              If a dog chews shoes whose dog chews shoes whose dog chews shoes whose
+              dog chews shoes whose dog chews shoes whose shoes does he choose?
+              If a dog chews shoes whose dog chews shoes whose dog chews shoes whose
+              dog chews shoes whose dog chews shoes whose shoes does he choose?
+              If a dog chews shoes whose dog chews shoes whose dog chews shoes whose
+              dog chews shoes whose dog chews shoes whose shoes does he choose?
+              If a dog chews shoes whose dog chews shoes whose dog chews shoes whose
+              dog chews shoes whose dog chews shoes whose shoes does he choose?
+            </p>
+          </div>
+          <figure className="mb-4">
+            <img
+              src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+              alt="Shoes"
+            />
+          </figure>
         </div>
-      </div>
-    </main>
-  );
-}
+      </main>
+    );
+  }
+  
